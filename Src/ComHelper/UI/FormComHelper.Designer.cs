ace75ff3c343983446ae1ba46f10a6c4acc290d9
Ace <258613763@qq.com>
2018-04-04 15:30:43 +0800

    增加文件

---
+++ Src/ComHelper/UI/FormComHelper.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace ComHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.cbBaundrate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbName = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.gbReceive = new System.Windows.Forms.GroupBox();
+            this.txtReceive = new System.Windows.Forms.RichTextBox();
+            this.gbSend = new System.Windows.Forms.GroupBox();
+            this.numSleep = new System.Windows.Forms.NumericUpDown();
+            this.txtSend = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.numMutilSend = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.gbReceive.SuspendLayout();
+            this.gbSend.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSleep)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMutilSend)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel5});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 364);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(592, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(81, 17);
+            this.toolStripStatusLabel1.Text = "COM1 Open";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(11, 17);
+            this.toolStripStatusLabel2.Text = "|";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(32, 17);
+            this.toolStripStatusLabel3.Text = "Rx:0";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(11, 17);
+            this.toolStripStatusLabel4.Text = "|";
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(31, 17);
+            this.toolStripStatusLabel5.Text = "Tx:0";
+            // 
+            // cbBaundrate
+            // 
+            this.cbBaundrate.FormattingEnabled = true;
+            this.cbBaundrate.Location = new System.Drawing.Point(330, 12);
+            this.cbBaundrate.Name = "cbBaundrate";
+            this.cbBaundrate.Size = new System.Drawing.Size(72, 20);
+            this.cbBaundrate.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(282, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "波特率：";
+            // 
+            // cbName
+            // 
+            this.cbName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbName.FormattingEnabled = true;
+            this.cbName.Location = new System.Drawing.Point(46, 12);
+            this.cbName.Name = "cbName";
+            this.cbName.Size = new System.Drawing.Size(230, 20);
+            this.cbName.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "端口：";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(456, 7);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(67, 29);
+            this.btnConnect.TabIndex = 12;
+            this.btnConnect.Text = "打开";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // gbReceive
+            // 
+            this.gbReceive.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbReceive.Controls.Add(this.txtReceive);
+            this.gbReceive.Location = new System.Drawing.Point(3, 39);
+            this.gbReceive.Name = "gbReceive";
+            this.gbReceive.Size = new System.Drawing.Size(577, 241);
+            this.gbReceive.TabIndex = 13;
+            this.gbReceive.TabStop = false;
+            this.gbReceive.Text = "接收区：已接收0字节";
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtReceive.HideSelection = false;
+            this.txtReceive.Location = new System.Drawing.Point(3, 17);
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.Size = new System.Drawing.Size(571, 221);
+            this.txtReceive.TabIndex = 1;
+            this.txtReceive.Text = "";
+            // 
+            // gbSend
+            // 
+            this.gbSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbSend.Controls.Add(this.numSleep);
+            this.gbSend.Controls.Add(this.txtSend);
+            this.gbSend.Controls.Add(this.btnSend);
+            this.gbSend.Controls.Add(this.numMutilSend);
+            this.gbSend.Controls.Add(this.label1);
+            this.gbSend.Controls.Add(this.label7);
+            this.gbSend.Location = new System.Drawing.Point(3, 283);
+            this.gbSend.Name = "gbSend";
+            this.gbSend.Size = new System.Drawing.Size(577, 84);
+            this.gbSend.TabIndex = 14;
+            this.gbSend.TabStop = false;
+            this.gbSend.Text = "发送区：已发送0字节";
+            // 
+            // numSleep
+            // 
+            this.numSleep.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numSleep.Location = new System.Drawing.Point(463, 49);
+            this.numSleep.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numSleep.Name = "numSleep";
+            this.numSleep.Size = new System.Drawing.Size(52, 21);
+            this.numSleep.TabIndex = 16;
+            this.numSleep.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numSleep.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // txtSend
+            // 
+            this.txtSend.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSend.HideSelection = false;
+            this.txtSend.Location = new System.Drawing.Point(0, 19);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(422, 59);
+            this.txtSend.TabIndex = 2;
+            this.txtSend.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(521, 18);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(50, 61);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // numMutilSend
+            // 
+            this.numMutilSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numMutilSend.Location = new System.Drawing.Point(463, 22);
+            this.numMutilSend.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numMutilSend.Name = "numMutilSend";
+            this.numMutilSend.Size = new System.Drawing.Size(52, 21);
+            this.numMutilSend.TabIndex = 14;
+            this.numMutilSend.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numMutilSend.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(428, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "间隔：";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(428, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "次数：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 386);
+            this.Controls.Add(this.gbSend);
+            this.Controls.Add(this.gbReceive);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cbBaundrate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "串口助手";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.gbReceive.ResumeLayout(false);
+            this.gbSend.ResumeLayout(false);
+            this.gbSend.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSleep)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMutilSend)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.ComboBox cbBaundrate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.GroupBox gbReceive;
+        private System.Windows.Forms.RichTextBox txtReceive;
+        private System.Windows.Forms.GroupBox gbSend;
+        private System.Windows.Forms.NumericUpDown numSleep;
+        private System.Windows.Forms.RichTextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.NumericUpDown numMutilSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
