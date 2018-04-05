5c93d4f353d9e0f6d3edb1370d160501e39c2409
Ace <258613763@qq.com>
2018-04-05 09:28:13 +0800

    代码整理

--- Src/ComHelper/UI/FormComHelper.cs
+++ Src/ComHelper/UI/FormComHelper.cs
@@ -18,10 +18,12 @@ namespace ComHelper
 
         private void FormComHelper_Load(object sender, EventArgs e)
         {
+            LoadInfo();
+        }
+        public void LoadInfo()
+        {
             var bs = new List<Int32>(new Int32[] { 1200, 2400, 4800, 9600, 14400, 19200, 38400, 56000, 57600, 115200, 128000, 194000, 256000, 512000, 1024000, 2048000 });
             cbBaundrate.DataSource = bs;
-
-
         }
     }
 }
