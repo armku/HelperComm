ae727c32769d32bd3229bd9419d9456fbd498a2b
Ace <258613763@qq.com>
2018-04-05 16:04:40 +0800

    代码拆分

--- Src/ComHelper/UI/FormComHelper.cs
+++ Src/ComHelper/UI/FormComHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+﻿using ComHelper.Net;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -52,7 +53,7 @@ namespace ComHelper
         /// <summary>下拉框显示串口</summary>
         public void ShowPorts()
         {
-            var ps = GetPortNames();
+            var ps =SerialTransport.GetPortNames();
             var str = String.Join(",", ps);
             // 如果端口有所改变，则重新绑定
             if (_ports != str)
@@ -67,70 +68,7 @@ namespace ComHelper
                 });
             }
         }
-        /// <summary>获取带有描述的串口名，没有时返回空数组</summary>
-        /// <returns></returns>
-        public static String[] GetPortNames()
-        {
-            var list = new List<String>();
-            foreach (var item in GetNames())
-            {
-                list.Add(String.Format("{0}({1})", item.Key, item.Value));
-            }
-            return list.ToArray();
-        }
-
-        /// <summary>获取串口列表，名称和描述</summary>
-        /// <returns></returns>
-        public static Dictionary<String, String> GetNames()
-        {
-            var dic = new Dictionary<String, String>(StringComparer.OrdinalIgnoreCase);
-            using (var key = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DEVICEMAP\SERIALCOMM", false))
-            using (var usb = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Enum\USB", false))
-            {
-                if (key != null)
-                {
-                    foreach (var item in key.GetValueNames())
-                    {
-                        var name = key.GetValue(item) + "";
-                        var des = "";
-
-                        // 尝试枚举USB串口
-                        foreach (var vid in usb.GetSubKeyNames())
-                        {
-                            var usbvid = usb.OpenSubKey(vid);
-                            foreach (var elm in usbvid.GetSubKeyNames())
-                            {
-                                var sub = usbvid.OpenSubKey(elm);
-                                //if (sub.GetValue("Class") + "" == "Ports")
-                                {
-                                    var FriendlyName = sub.GetValue("FriendlyName") + "";
-                                    if (FriendlyName.Contains("({0})".F(name)))
-                                    {
-                                        des = FriendlyName.TrimEnd("({0})".F(name)).Trim();
-                                        break;
-                                    }
-                                }
-                            }
-                            if (!des.IsNullOrEmpty()) break;
-                        }
-
-                        // 最后选择设备映射的串口名
-                        if (des.IsNullOrEmpty())
-                        {
-                            des = item;
-                            var p = item.LastIndexOf('\\');
-                            if (p >= 0) des = des.Substring(p + 1);
-                        }
-
-                        //dic.Add(name, des);
-                        // 某台机器上发现，串口有重复
-                        dic[name] = des;
-                    }
-                }
-            }
-            return dic;
-        }
-
+        
         private void btnConnect_Click(object sender, EventArgs e)
         {
             var btn = sender as Button;
