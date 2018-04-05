d8fdbed59beb67f662b782e4a010f9fb6a377983
Ace <258613763@qq.com>
2018-04-05 09:40:40 +0800

    端口号显示正常

--- Src/ComHelper/UI/FormComHelper.cs
+++ Src/ComHelper/UI/FormComHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,8 +23,91 @@ namespace ComHelper
         }
         public void LoadInfo()
         {
+            ShowPorts();
             var bs = new List<Int32>(new Int32[] { 1200, 2400, 4800, 9600, 14400, 19200, 38400, 56000, 57600, 115200, 128000, 194000, 256000, 512000, 1024000, 2048000 });
             cbBaundrate.DataSource = bs;
         }
+        String _ports = null;
+        /// <summary>下拉框显示串口</summary>
+        public void ShowPorts()
+        {
+            var ps = GetPortNames();
+            var str = String.Join(",", ps);
+            // 如果端口有所改变，则重新绑定
+            if (_ports != str)
+            {
+                _ports = str;
+
+                this.Invoke(() =>
+                {
+                    var old = cbName.SelectedItem + "";
+                    cbName.DataSource = ps;
+                    if (!String.IsNullOrEmpty(old) && Array.IndexOf(ps, old) >= 0) cbName.SelectedItem = old;
+                });
+            }
+        }
+        /// <summary>获取带有描述的串口名，没有时返回空数组</summary>
+        /// <returns></returns>
+        public static String[] GetPortNames()
+        {
+            var list = new List<String>();
+            foreach (var item in GetNames())
+            {
+                list.Add(String.Format("{0}({1})", item.Key, item.Value));
+            }
+            return list.ToArray();
+        }
+
+        /// <summary>获取串口列表，名称和描述</summary>
+        /// <returns></returns>
+        public static Dictionary<String, String> GetNames()
+        {
+            var dic = new Dictionary<String, String>(StringComparer.OrdinalIgnoreCase);
+            using (var key = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DEVICEMAP\SERIALCOMM", false))
+            using (var usb = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Enum\USB", false))
+            {
+                if (key != null)
+                {
+                    foreach (var item in key.GetValueNames())
+                    {
+                        var name = key.GetValue(item) + "";
+                        var des = "";
+
+                        // 尝试枚举USB串口
+                        foreach (var vid in usb.GetSubKeyNames())
+                        {
+                            var usbvid = usb.OpenSubKey(vid);
+                            foreach (var elm in usbvid.GetSubKeyNames())
+                            {
+                                var sub = usbvid.OpenSubKey(elm);
+                                //if (sub.GetValue("Class") + "" == "Ports")
+                                {
+                                    var FriendlyName = sub.GetValue("FriendlyName") + "";
+                                    if (FriendlyName.Contains("({0})".F(name)))
+                                    {
+                                        des = FriendlyName.TrimEnd("({0})".F(name)).Trim();
+                                        break;
+                                    }
+                                }
+                            }
+                            if (!des.IsNullOrEmpty()) break;
+                        }
+
+                        // 最后选择设备映射的串口名
+                        if (des.IsNullOrEmpty())
+                        {
+                            des = item;
+                            var p = item.LastIndexOf('\\');
+                            if (p >= 0) des = des.Substring(p + 1);
+                        }
+
+                        //dic.Add(name, des);
+                        // 某台机器上发现，串口有重复
+                        dic[name] = des;
+                    }
+                }
+            }
+            return dic;
+        }        
     }
 }
---
+++ Src/ComHelper/UI/Utility.cs
@@ -0,0 +1,328 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+
+namespace System
+{
+    /// <summary>工具类</summary>
+    /// <remarks>
+    /// 采用静态架构，允许外部重载工具类的各种实现<seealso cref="DefaultConvert"/>。
+    /// 所有类型转换均支持默认值，默认值为该default(T)，在转换失败时返回默认值。
+    /// </remarks>
+    public static class Utility
+    {
+        #region 类型转换
+        /// <summary>类型转换提供者</summary>
+        /// <remarks>重载默认提供者<seealso cref="DefaultConvert"/>并赋值给<see cref="Convert"/>可改变所有类型转换的行为</remarks>
+        public static DefaultConvert Convert { get; set; } = new DefaultConvert();
+
+        /// <summary>转为整数，转换失败时返回默认值。支持字符串、全角、字节数组（小端）</summary>
+        /// <remarks>Int16/UInt32/Int64等，可以先转为最常用的Int32后再二次处理</remarks>
+        /// <param name="value">待转换对象</param>
+        /// <param name="defaultValue">默认值。待转换对象无效时使用</param>
+        /// <returns></returns>
+        public static Int32 ToInt(this Object value, Int32 defaultValue = 0) { return Convert.ToInt(value, defaultValue); }
+
+        /// <summary>转为浮点数，转换失败时返回默认值。支持字符串、全角、字节数组（小端）</summary>
+        /// <remarks>Single可以先转为最常用的Double后再二次处理</remarks>
+        /// <param name="value">待转换对象</param>
+        /// <param name="defaultValue">默认值。待转换对象无效时使用</param>
+        /// <returns></returns>
+        public static Double ToDouble(this Object value, Double defaultValue = 0) { return Convert.ToDouble(value, defaultValue); }
+
+        /// <summary>转为布尔型，转换失败时返回默认值。支持大小写True/False、0和非零</summary>
+        /// <param name="value">待转换对象</param>
+        /// <param name="defaultValue">默认值。待转换对象无效时使用</param>
+        /// <returns></returns>
+        public static Boolean ToBoolean(this Object value, Boolean defaultValue = false) { return Convert.ToBoolean(value, defaultValue); }
+
+        /// <summary>转为时间日期，转换失败时返回最小时间</summary>
+        /// <param name="value">待转换对象</param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(this Object value) { return Convert.ToDateTime(value, DateTime.MinValue); }
+
+        /// <summary>时间日期转为yyyy-MM-dd HH:mm:ss完整字符串</summary>
+        /// <remarks>最常用的时间日期格式，可以无视各平台以及系统自定义的时间格式</remarks>
+        /// <param name="value">待转换对象</param>
+        /// <returns></returns>
+        public static String ToFullString(this DateTime value) { return Convert.ToFullString(value); }
+
+        #endregion
+
+        #region 异常处理
+        /// <summary>获取内部真实异常</summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public static Exception GetTrue(this Exception ex) { return Convert.GetTrue(ex); }
+        #endregion
+    }
+
+    /// <summary>默认转换</summary>
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    public class DefaultConvert
+    {
+        /// <summary>转为整数</summary>
+        /// <param name="value">待转换对象</param>
+        /// <param name="defaultValue">默认值。待转换对象无效时使用</param>
+        /// <returns></returns>
+        public virtual Int32 ToInt(Object value, Int32 defaultValue)
+        {
+            if (value == null || value == DBNull.Value) return defaultValue;
+
+            // 特殊处理字符串，也是最常见的
+            if (value is String str)
+            {
+                // 拷贝而来的逗号分隔整数
+                str = str.Replace(",", null);
+                str = ToDBC(str).Trim();
+                if (str.IsNullOrEmpty()) return defaultValue;
+
+                if (Int32.TryParse(str, out var n)) return n;
+                return defaultValue;
+            }
+            else if (value is Byte[] buf)
+            {
+                if (buf == null || buf.Length < 1) return defaultValue;
+
+                switch (buf.Length)
+                {
+                    case 1:
+                        return buf[0];
+                    case 2:
+                        return BitConverter.ToInt16(buf, 0);
+                    case 3:
+                        return BitConverter.ToInt32(new Byte[] { buf[0], buf[1], buf[2], 0 }, 0);
+                    case 4:
+                        return BitConverter.ToInt32(buf, 0);
+                    default:
+                        break;
+                }
+            }
+
+            try
+            {
+                return Convert.ToInt32(value);
+            }
+            catch { return defaultValue; }
+        }
+
+        /// <summary>转为浮点数</summary>
+        /// <param name="value">待转换对象</param>
+        /// <param name="defaultValue">默认值。待转换对象无效时使用</param>
+        /// <returns></returns>
+        public virtual Double ToDouble(Object value, Double defaultValue)
+        {
+            if (value == null || value == DBNull.Value) return defaultValue;
+
+            // 特殊处理字符串，也是最常见的
+            if (value is String str)
+            {
+                str = ToDBC(str).Trim();
+                if (str.IsNullOrEmpty()) return defaultValue;
+
+                if (Double.TryParse(str, out var n)) return n;
+                return defaultValue;
+            }
+            else if (value is Byte[] buf)
+            {
+                if (buf == null || buf.Length < 1) return defaultValue;
+
+                switch (buf.Length)
+                {
+                    case 1:
+                        return buf[0];
+                    case 2:
+                        return BitConverter.ToInt16(buf, 0);
+                    case 3:
+                        return BitConverter.ToInt32(new Byte[] { buf[0], buf[1], buf[2], 0 }, 0);
+                    case 4:
+                        return BitConverter.ToInt32(buf, 0);
+                    default:
+                        // 凑够8字节
+                        if (buf.Length < 8)
+                        {
+                            var bts = new Byte[8];
+                            Buffer.BlockCopy(buf, 0, bts, 0, buf.Length);
+                            buf = bts;
+                        }
+                        return BitConverter.ToDouble(buf, 0);
+                }
+            }
+
+            try
+            {
+                return Convert.ToDouble(value);
+            }
+            catch { return defaultValue; }
+        }
+
+        /// <summary>转为布尔型。支持大小写True/False、0和非零</summary>
+        /// <param name="value">待转换对象</param>
+        /// <param name="defaultValue">默认值。待转换对象无效时使用</param>
+        /// <returns></returns>
+        public virtual Boolean ToBoolean(Object value, Boolean defaultValue)
+        {
+            if (value == null || value == DBNull.Value) return defaultValue;
+
+            // 特殊处理字符串，也是最常见的
+            if (value is String str)
+            {
+                str = ToDBC(str).Trim();
+                if (str.IsNullOrEmpty()) return defaultValue;
+
+                if (Boolean.TryParse(str, out var b)) return b;
+
+                if (String.Equals(str, Boolean.TrueString, StringComparison.OrdinalIgnoreCase)) return true;
+                if (String.Equals(str, Boolean.FalseString, StringComparison.OrdinalIgnoreCase)) return false;
+
+                // 特殊处理用数字0和1表示布尔型
+                if (Int32.TryParse(str, out var n)) return n > 0;
+
+                return defaultValue;
+            }
+
+            try
+            {
+                return Convert.ToBoolean(value);
+            }
+            catch { return defaultValue; }
+        }
+
+        /// <summary>转为时间日期</summary>
+        /// <param name="value">待转换对象</param>
+        /// <param name="defaultValue">默认值。待转换对象无效时使用</param>
+        /// <returns></returns>
+        public virtual DateTime ToDateTime(Object value, DateTime defaultValue)
+        {
+            if (value == null || value == DBNull.Value) return defaultValue;
+
+            // 特殊处理字符串，也是最常见的
+            if (value is String str)
+            {
+                str = ToDBC(str).Trim();
+                if (str.IsNullOrEmpty()) return defaultValue;
+
+                if (DateTime.TryParse(str, out var n)) return n;
+                if (str.Contains("-") && DateTime.TryParseExact(str, "yyyy-M-d", null, DateTimeStyles.None, out n)) return n;
+                if (str.Contains("/") && DateTime.TryParseExact(str, "yyyy/M/d", null, DateTimeStyles.None, out n)) return n;
+                if (DateTime.TryParse(str, out n)) return n;
+                return defaultValue;
+            }
+
+            try
+            {
+                return Convert.ToDateTime(value);
+            }
+            catch { return defaultValue; }
+        }
+
+        /// <summary>全角为半角</summary>
+        /// <remarks>全角半角的关系是相差0xFEE0</remarks>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        String ToDBC(String str)
+        {
+            var ch = str.ToCharArray();
+            for (var i = 0; i < ch.Length; i++)
+            {
+                // 全角空格
+                if (ch[i] == 0x3000)
+                    ch[i] = (Char)0x20;
+                else if (ch[i] > 0xFF00 && ch[i] < 0xFF5F)
+                    ch[i] = (Char)(ch[i] - 0xFEE0);
+            }
+            return new String(ch);
+        }
+
+        /// <summary>时间日期转为yyyy-MM-dd HH:mm:ss完整字符串</summary>
+        /// <param name="value">待转换对象</param>
+        /// <param name="emptyValue">字符串空值时显示的字符串，null表示原样显示最小时间，String.Empty表示不显示</param>
+        /// <returns></returns>
+        public virtual String ToFullString(DateTime value, String emptyValue = null)
+        {
+            if (emptyValue != null && value <= DateTime.MinValue) return emptyValue;
+
+            //return value.ToString("yyyy-MM-dd HH:mm:ss");
+
+            //var dt = value;
+            //var sb = new StringBuilder();
+            //sb.Append(dt.Year.ToString().PadLeft(4, '0'));
+            //sb.Append("-");
+            //sb.Append(dt.Month.ToString().PadLeft(2, '0'));
+            //sb.Append("-");
+            //sb.Append(dt.Day.ToString().PadLeft(2, '0'));
+            //sb.Append(" ");
+
+            //sb.Append(dt.Hour.ToString().PadLeft(2, '0'));
+            //sb.Append(":");
+            //sb.Append(dt.Minute.ToString().PadLeft(2, '0'));
+            //sb.Append(":");
+            //sb.Append(dt.Second.ToString().PadLeft(2, '0'));
+
+            //return sb.ToString();
+
+            var cs = "yyyy-MM-dd HH:mm:ss".ToCharArray();
+
+            var k = 0;
+            var y = value.Year;
+            cs[k++] = (Char)('0' + (y / 1000));
+            y %= 1000;
+            cs[k++] = (Char)('0' + (y / 100));
+            y %= 100;
+            cs[k++] = (Char)('0' + (y / 10));
+            y %= 10;
+            cs[k++] = (Char)('0' + y);
+            k++;
+
+            var m = value.Month;
+            cs[k++] = (Char)('0' + (m / 10));
+            m %= 10;
+            cs[k++] = (Char)('0' + m);
+            k++;
+
+            m = value.Day;
+            cs[k++] = (Char)('0' + (m / 10));
+            m %= 10;
+            cs[k++] = (Char)('0' + m);
+            k++;
+
+            m = value.Hour;
+            cs[k++] = (Char)('0' + (m / 10));
+            m %= 10;
+            cs[k++] = (Char)('0' + m);
+            k++;
+
+            m = value.Minute;
+            cs[k++] = (Char)('0' + (m / 10));
+            m %= 10;
+            cs[k++] = (Char)('0' + m);
+            k++;
+
+            m = value.Second;
+            cs[k++] = (Char)('0' + (m / 10));
+            m %= 10;
+            cs[k++] = (Char)('0' + m);
+            k++;
+
+            return new String(cs);
+        }
+        /// <summary>获取内部真实异常</summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public virtual Exception GetTrue(Exception ex)
+        {
+            if (ex == null) return null;
+
+            if (ex is AggregateException)
+                return GetTrue((ex as AggregateException).Flatten().InnerException);
+
+            if (ex is TargetInvocationException)
+                return GetTrue((ex as TargetInvocationException).InnerException);
+
+            if (ex is TypeInitializationException)
+                return GetTrue((ex as TypeInitializationException).InnerException);
+
+            return ex;
+        }
+    }
+}
