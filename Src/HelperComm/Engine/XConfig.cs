9d9fbeb6b5b3b27355cc3ada90ca938fcdc4d404
Ace <258613763@qq.com>
2018-11-17 21:13:00 +0800

    del

--- Src/HelperComm/Engine/XConfig.cs
+++ Src/HelperComm/Engine/XConfig.cs
@@ -18,11 +18,7 @@ namespace XCoder
         /// <summary>日志着色</summary>
         [Description("日志着色")]
         public Boolean ColorLog { get; set; } = true;
-
-        
-        /// <summary>更新服务器</summary>
-        [Description("更新服务器")]
-        public String UpdateServer { get; set; } = "";
+                
         #endregion
 
         #region 加载/保存
@@ -31,9 +27,7 @@ namespace XCoder
         }
 
         protected override void OnLoaded()
-        {
-            if (UpdateServer.IsNullOrEmpty() || UpdateServer.EqualIgnoreCase("http://x.newlifex.com/")) UpdateServer = NewLife.Setting.Current.PluginServer;
-
+        {            
             base.OnLoaded();
         }
         #endregion
